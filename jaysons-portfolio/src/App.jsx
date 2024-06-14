fe3e94638f23cda1c66836ff2fab935b95a58656
JaysonNunez1 <jaysonnunez3@gmail.com>
2024-06-13 23:13:44 -0400

    worked on the projects section

--- jaysons-portfolio/src/App.jsx
+++ jaysons-portfolio/src/App.jsx
@@ -5,6 +5,7 @@ import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
 import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects";
 
 
 
@@ -14,6 +15,7 @@ function App() {
       <Hero />
       <About />
       <Experience />
+      <Projects />
     </div>
   );
 }
